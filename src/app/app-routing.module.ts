fc19cbcf02d56785e1963de2d2758f4a8d1b7449
Barend Erasmus <barend.erasmus@euromonitor.com>
2019-04-17 10:43:22 +0200

    implement demo identity server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,9 +35,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {
-      useHash: true,
-    }),
+    RouterModule.forRoot(routes),
   ],
   exports: [RouterModule],
 })
