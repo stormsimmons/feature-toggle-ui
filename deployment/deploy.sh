766bfeec8897cbd2d3b72148a236bef9d0c865e1
Barend Erasmus <Barend.Erasmus@Euromonitor.com>
2019-06-03 11:30:19 +0200

    update

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -19,3 +19,9 @@ cd feature-toggle-ui
 docker build --no-cache -t app-foggle-io .
 docker run -d --name app-foggle-io -p 8082:80 app-foggle-io
 cd ..
+
+git clone https://github.com/xyzblocks/feature-toggle-nodejs.git
+cd feature-toggle-nodejs
+docker build --no-cache -t feature-toggle-nodejs .
+docker run -d --name feature-toggle-nodejs -p 8083:8080 -e "AUDIENCE=SJytKyPQ5eiPtmIrxdmFmun3JKtXmkAR" -e "AUTHORITY=https://xyzblocks.auth0.com" -e "CONNECTION_STRING=" -e "HOST=0.0.0.0"  feature-toggle-nodejs
+cd ..
