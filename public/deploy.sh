38f5dd97542e0d55a42551f93ff4832d369655b4
Barend Erasmus <Barend.Erasmus@Euromonitor.com>
2019-06-05 14:51:56 +0200

    remove site and deployment scripts

--- public/deploy.sh
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/xyzblocks/feature-toggle-ui.git
-
-cd feature-toggle-ui/public
-
-docker build --no-cache -t foggle-io .
-
-docker run -d --name foggle-io -p 8081:80 foggle-io
-
-sudo letsencrypt certonly --standalone --email xyzblocks@gmail.com -d foggle.io
